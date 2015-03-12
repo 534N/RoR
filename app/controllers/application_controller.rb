a131f66769fa818d7d7c2af3f43a4a23b6101148
Xiaofan Yang <xiaofanyang@Xiaofans-MacBook-Pro.local>
2015-03-12 10:59:50 -0400

    displaying the schedule

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,29 +7,4 @@ class ApplicationController < ActionController::Base
   def hello
   	render text: 'hello, world'
   end
-
-  def parse
-  	require 'open-uri'
-  	require 'pp'
-
-  	page = Nokogiri::HTML(open("http://www.cheapthrills.ca/news.html"))
-  	rows = page.xpath("//table[@border]/tr")
-
-  	details = rows.collect do |row|
-	  detail = {}
-	  [
-	    [:status, 'td[1]/font/text()'],
-	    [:artist, 'td[2]/font/text()'],
-	    [:date, 'td[3]/font/text()'],
-	    [:time, 'td[4]/font/text()'],
-	    [:venue, 'td[5]/font/text()'],
-	    [:price, 'td[6]/font/text()']
-	  ].each do |name, xpath|
-	  	detail[name] = row.at_xpath(xpath).to_s.strip
-	  end
-	  detail
-	end
-	pp details
-
-  end
 end
---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,29 @@
+class TestController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def parse
+  	require 'open-uri'
+  	require 'pp'
+
+  	page = Nokogiri::HTML(open("http://www.cheapthrills.ca/news.html"))
+  	rows = page.xpath("//table[@border]/tr")
+
+  	@details = rows.collect do |row|
+	  detail = {}
+	  [
+	    [:status, 'td[1]/font/text()'],
+	    [:artist, 'td[2]/font/text()'],
+	    [:date, 'td[3]/font/text()'],
+	    [:time, 'td[4]/font/text()'],
+	    [:venue, 'td[5]/font/text()'],
+	    [:price, 'td[6]/font/text()']
+	  ].each do |name, xpath|
+	  	detail[name] = row.at_xpath(xpath).to_s.strip
+	  end
+	  detail
+	end
+
+  end
+end
