445e45b6d1bd010b34e34800c22b2de02f1ad148
Xiaofan Yang <xiaofanyang@Xiaofans-MacBook-Pro.local>
2015-03-13 17:04:30 -0400

    get ready to process artist panel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,33 +16,105 @@
 //= require_tree .
 
 (function() {
+	var clientID = '998651536d9a2dcea17749d75862ef39';
+
+	SC.initialize({
+	 	client_id: 	clientID
+	});
+
 	var ele = document.getElementById('music-app');
+	var _artistBuffer = {};
 
 	ele.addEventListener('click', function(e) {
-		console.log(e.target)
-		var artistString = e.target.innerHTML.trim();
-		var artistList = new Array();
+		if (e.target.className.match(/artist/)) {
+			var artistString = e.target.innerHTML.trim();
+			var artistList = new Array();
 
-		artistList = artistString.split(/\s+[\+|with|\||\/]\s+/g);
+			if (artistString.match(/with/)) {
+				artistList = artistString.split(/\s+with\s+/);
+			} else {
+				artistList = artistString.split(/\s?[\+|\||\/]\s?/g);
+			}
 
-		for(var i=0; i<artistList.length; i++) {
-			var artist_id = artistList[i].replace(/\s/g, '');
-			var user_url = ajax(artist_id);
-
+			for(var i=0; i<artistList.length; i++) {
+				var artist_name = artistList[i];
+				get_user(artist_name, e.target, displayAP);
+			}
 		}
 	});
 
-	function ajax(artist_id) {
-		var url = "http://api.soundcloud.com/users/" + artist_id.toLowerCase() + ".json?client_id=998651536d9a2dcea17749d75862ef39";
+	function get_user(artist_name, target, callback) {
 
+		if (_artistBuffer.hasOwnProperty(artist_name)) {
+			console.log('>>>>>>>>>>>>>>', _artistBuffer[artist_name])
+
+		} else {
+			var ap = new ArtistPanel(artist_name);
+			var artist_username = artist_name.replace(/\s/g, '').toLowerCase();
+
+			var url = "http://api.soundcloud.com/users/" + artist_username + ".json?client_id=" + clientID;
+			ajax(url, function(err, response) {
+				if (err) {
+					displayWarning('Unable to identify artist: ' + ap.artist, target);
+				} else {
+					ap.updateUserInfo(response);
+					_artistBuffer[artist_name] = ap;
+					findTracks(target, ap, callback);
+				}
+			});
+		}
+	}
+
+	function findTracks(target, ap, callback) {
+		var url = "http://api.soundcloud.com/users/" + ap.artistID + "/tracks.json?client_id=" + clientID;
+		ajax(url, function(err, response) {
+			if (err) {
+				// 
+			} else {
+				ap.updateTrackInfo(response);
+			}
+		});
+	}
+
+
+	function displayWarning(msg, target) {
+		var wrapper = document.createElement('div');
+		wrapper.className = 'warning twelve columns'
+
+		var text = document.createTextNode(msg);
+		wrapper.appendChild(text);
+
+		target.parentNode.appendChild(wrapper);
+
+		setTimeout(function() {
+			target.parentNode.removeChild(wrapper);
+		}, 3000);
+	}
+
+	function displayAP(target, ap) {
+		// 
+	}
+
+	function ajax(url, callback) {
 		var r = new XMLHttpRequest();
 		r.open("GET", url, true);
+		
+
 		r.onreadystatechange = function () {
-			if (r.readyState != 4 || r.status != 200) return;
-			console.log("Success: ", JSON.parse(r.responseText));
+			if (r.readyState == 4) {
+				if (r.status == 200 ) {
+					callback(null, JSON.parse(r.responseText));
+				} else {
+					callback(r.status, null);
+				}
+			} else {
+				return;
+			}
 		};
+
 		r.send();
 	}
+
 
 	
 })();
